69c78b72ee05ef9f683154356ad2ed0bdcbe2c36
ivin <ivinrodriguees@gmail.com>
2020-11-13 22:47:19 -0300

    Resolução do bug do dano

--- src/app/game/player-interface/interface-battle/interface-battle.component.ts
+++ src/app/game/player-interface/interface-battle/interface-battle.component.ts
@@ -12,7 +12,8 @@ export class InterfaceBattleComponent implements OnInit {
   @Input() enemy: any
   @Output() battleResult: EventEmitter<boolean> = new EventEmitter()
   player: any
-  d20Result: number
+  diceResult: number
+  diceNumberVisibility: boolean = false
 
 
   constructor( private playerService: PlayerSerivce,
@@ -36,7 +37,8 @@ export class InterfaceBattleComponent implements OnInit {
 
     setTimeout( () => {
 
-      this.rollD20(1, 20)
+      this.diceNumberVisibility = true
+      this.rollDice(1, 20)
 
       setTimeout( () => {
 
@@ -60,33 +62,32 @@ export class InterfaceBattleComponent implements OnInit {
 
       setTimeout( ()=> {
 
-        if(this.d20Result == 1) {
+        if(this.diceResult == 1) {
 
           alert('bruh')
 
         }
-        else if(this.d20Result >= 2 && this.d20Result <= 5) {
+        else if(this.diceResult >= 2 && this.diceResult <= 5) {
 
           this.enemyService.takeDamage(this.enemy.id ,this.player.damage)
 
         }
-        else if(this.d20Result >= 5 && this.d20Result <= 10) {
+        else if(this.diceResult >= 5 && this.diceResult <= 10) {
 
           this.enemyService.takeDamage(this.enemy.id ,this.player.damage)
 
         }
-        else if(this.d20Result >= 10 && this.d20Result <= 15) {
+        else if(this.diceResult >= 10 && this.diceResult <= 15) {
 
           this.enemyService.takeDamage(this.enemy.id ,this.player.damage)
 
         }
-        else if(this.d20Result >= 15 && this.d20Result <= 19) {
+        else if(this.diceResult >= 15 && this.diceResult <= 19) {
 
           this.enemyService.takeDamage(this.enemy.id ,this.player.damage)
 
         }
-
-        else {
+        else{
 
           alert('ultimate')
 
@@ -121,27 +122,27 @@ export class InterfaceBattleComponent implements OnInit {
 
       setTimeout( ()=> {
 
-        if(this.d20Result == 1) {
+        if(this.diceResult == 1) {
 
           alert('bruh')
 
         }
-        else if(this.d20Result >= 2 && this.d20Result <= 5) {
+        else if(this.diceResult >= 2 && this.diceResult <= 5) {
 
           this.playerService.takeDamage(this.enemy.damage)
 
         }
-        else if(this.d20Result >= 5 && this.d20Result <= 10) {
+        else if(this.diceResult >= 5 && this.diceResult <= 10) {
 
           this.playerService.takeDamage(this.enemy.damage)
 
         }
-        else if(this.d20Result >= 10 && this.d20Result <= 15) {
+        else if(this.diceResult >= 10 && this.diceResult <= 15) {
 
           this.playerService.takeDamage(this.enemy.damage)
 
         }
-        else if(this.d20Result >= 15 && this.d20Result <= 19) {
+        else if(this.diceResult >= 15 && this.diceResult <= 19) {
 
           this.playerService.takeDamage(this.enemy.damage)
 
@@ -169,7 +170,7 @@ export class InterfaceBattleComponent implements OnInit {
 
     }
 
-    this.d20Result = null
+    this.diceNumberVisibility = false
 
   }
 
@@ -183,9 +184,9 @@ export class InterfaceBattleComponent implements OnInit {
 
 
 
-  rollD20(min, max) {
+  rollDice(min, max) {
 
-    this.d20Result = Math.floor(Math.random() * (max - min) + min)
+    this.diceResult = Math.floor(Math.random() * (max - min) + min)
 
   }
 
